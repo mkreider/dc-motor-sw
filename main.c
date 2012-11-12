5ff7257d3fe4f10b3afbab7e31e3f1a88e323e87
Mathias Kreider <m.kreider@gsi.de>
2012-11-12 10:31:53 +0100

    project creation

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ * Created: 09.11.2012 14:59:06
+ *  Author: mkreider
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
