26a16561341e73b410bb8d42f8924dc27b938b2c
Philip87 <philip.burkhard@gmx.de>
2012-11-14 11:12:24 +0100

    added test program for median fiilter

---
+++ test_median.c
@@ -0,0 +1,17 @@
+/*
+ * test_median.c
+ *
+ * Created: 13.11.2012 13:45:06
+ *  Author: pburkhar
+ */ 
+#include <avr/io.h>
+#include "include/adc.h"
+#include "include/median.h"
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
