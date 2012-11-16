177d34929ec240106e5070ea8a4a6351a5ef6ca4
Philip87 <philip.burkhard@gmx.de>
2012-11-16 11:40:54 +0100

    fucking Atmel studio include as link bug forces to throw all files into the same folder

--- test_median.c
+++ test_median.c
@@ -5,13 +5,104 @@
  *  Author: pburkhar
  */ 
 #include <avr/io.h>
-#include "include/adc.h"
-#include "include/median.h"
+#include <util/delay.h>
+#include "include/uart.h"
+
+char text1[] = "Hallo Welt!";
+char text[]= "Nich schon wieder!";
+
+uint8_t counter = 0;
+uint8_t adc[]={4, 3, 5, 2, 1};
+
+
+void init(void)
+{
+	DDRB = 0b01111111;
+	DDRC  = 0x00;
+	PORTC &= ~(1<<PC0);
+	init_uart();
+	
+	return;		
+}
+
+
+
+// void median(void)
+// {
+	
+	
+void swap(uint8_t *a, uint8_t *b) 
+{
+    uint8_t temp;
+ 
+    temp = *a;
+    *a = *b;
+    *b = temp; 
+	
+	return; 
+}
+
+
+		
+		
+	
+void median(void)		
+{
+
+if(adc[0] > adc[1])
+{
+	swap(adc[0], adc[1]);
+}
+
+if(adc[2] > adc[3])
+{
+	swap(adc[2], adc[3]);
+}
+
+if(adc[1] > adc[3])
+{
+	swap(adc[0], adc[2]);
+	swap(adc[1], adc[3]);
+}
+
+if(adc[4] < adc[1])
+{
+	if(adc[4] < adc[0])
+{
+	swap(adc[0], adc[4]);
+	swap(adc[4], adc[1]);
+	swap(adc[4], adc[3]);
+}
+	else if(adc[4] < adc [3])
+	{
+		swap(adc[3], adc[3]);
+}	}
+
+if(adc[2] < adc[1])
+{	if(adc[2] < adc[0])
+	{
+		swap(adc[0], adc[2]);
+		swap(adc[1], adc[2]);
+	}		
+	else if(adc[2] > adc[3])
+	{
+		swap(adc[2], adc[3]);
+	}
+}		
+
+		}
+
+
+
 
 int main(void)
 {
+	init();
+	uint8_t i;
+	
     while(1)
     {
-        //TODO:: Please write your application code 
+        for(i=0;i<5;i++) uartputc(adc[i]);
+		median();	 
     }
 }
