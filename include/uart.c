0cfa96af238b21904f64d23d341e90bde5576a6a
Nadja <n.fischer@gsi.de>
2012-11-16 11:15:45 +0100

    Working output functions for uart.

---
+++ include/uart.c
@@ -0,0 +1,31 @@
+#include <avr/io.h>
+
+void uartputc(char wort)				
+{
+	while(!((UCSR0A)&(1<<UDRE0)));
+	UDR0=wort;
+}
+
+void uartput_uint16(uint16_t zahl)
+{
+	uint16_t div	=	10000;
+	uint16_t erg;							//erg_ ergebnis
+	uint8_t  fn = 1;						//fn= führende null
+	
+	while (div)
+	{
+		erg = (zahl/div);
+		if(erg != 0) fn = 0; 
+		if((!fn)|(div==1)) uartputc((char)(erg+48));		//ausgabe der zahl
+		
+		zahl %= div;
+		div /= 10;
+	}
+}	
+
+void init_uart()
+{
+	UCSR0B			=		(1<<RXEN0)  | (1<<TXEN0);		// RXEN und TXEN Enabled.
+	UCSR0C			=		(1<<UCSZ01) | (1<<UCSZ00);		// Asynchron, no parity, 1 stop bit, 8 bit size, no CK.	
+	UBRR0			=		0x08;							// Baud rate 57.6k
+}	
---
+++ include/uart.h
@@ -0,0 +1,8 @@
+#ifndef _UART_H_
+#define _UART_H_
+
+void uartputc(char wort);
+void uartput_uint16(uint16_t zahl);
+void init_uart(void);
+
+#endif
