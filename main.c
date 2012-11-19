4f5e43a22744b9e0fc595938d4d059021ac84c53
Nadja91 <f.nadja@gmx.net>
2012-11-19 14:21:10 +0100

    Improvment of the uart and edit a text.

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
  * main.c
  *
  * Created: 13.11.2012 13:45:06
- *  Author: pburkhar
- */ 
+  */ 
+
 #include <avr/io.h>
 #include "include/adc.h"
 #include "include/median.h"
@@ -14,4 +14,14 @@ int main(void)
     {
         //TODO:: Please write your application code 
     }
+}
+
+char text() = "Achtung Ueberstrom"
+
+void output (const char* text())				//nicht veränderbar wegen char das * ist zum anzeigen das es ein pointer ist
+{
+	while (*text!=0x00)							//soll aufhören wenn es 0x00 erreicht hat
+	{
+		uartputc (*(text++));		// damit alles angezeigt werden soll ++ 
+	}
 }
