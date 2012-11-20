1c89bfe66d5c3951d42adae6312a6bc76b587501
Philip87 <philip.burkhard@gmx.de>
2012-11-20 13:34:16 +0100

    subtleties did at the sort of 5 numbers

--- test_median.c
+++ test_median.c
@@ -10,12 +10,14 @@
 
 char text1[] = "Hallo Welt!";
 char text[]= "Nich schon wieder!";
-
+uint8_t rep[]={0};
 uint8_t counter = 0;
-//uint8_t adc []={1, 2, 3, 4, 5};   
-uint8_t adc[]={4, 3, 5, 2, 1};	
+uint16_t adc []={400, 1278, 2890, 1479, 9876};   
+/*uint8_t adc[]={4, 3, 5, 2, 1};*/	
 /*uint8_t	adc[]={5, 6, 7};*/
 uint8_t x;
+uint16_t c;
+uint8_t k =1;
 
 void init(void)
 {
@@ -27,9 +29,10 @@ void init(void)
 	return;		
 }
 
-void swap(uint8_t *a, uint8_t *b) 
+
+void swap(uint16_t *a, uint16_t *b) 
 {
-    uint8_t temp;
+    uint16_t temp;
  
     temp = *a;
     *a = *b;
@@ -38,176 +41,99 @@ void swap(uint8_t *a, uint8_t *b)
 	return; 
 }
 
-// void median(void)
-// {
-	
-	
+/*void median(void)*/		
 
-//### BUBBLESORT ###__________________________________________
-
-
- 
-void Bubble_Sort_Long(void)
-{
-/*	uint8_t timer = 0;*/
-/*	for(timer=0;timer<5;timer++)*/
-/*	{*/
-	if(adc[0]>adc[1]) swap(&adc[0], &adc[1]);
-	if(adc[1]>adc[2]) swap(&adc[1], &adc[2]);
-	if(adc[2]>adc[3]) swap(&adc[2], &adc[3]);
-	if(adc[3]>adc[4]) swap(&adc[3], &adc[4]);
-/*	if(adc[0]>adc[4]) swap(adc[4], adc[0]);*/
-/*	}*/
-	return;	
-}
-
-		
-	
-void median(void)		
-{
 //### NEUE SCHLEIFE (3 Zahlen) ###____________________________________
 
-	if(adc[0]<adc[1])
-	{	if(adc[0]<adc[2])
-		{	if(adc[1]<adc[2])
-			{	x = adc[1];				
-			}
-			else if(adc[1]>adc[2])
-			{	x = adc[2];				
-			}						
-		}				
-	}
-
-
-	if(adc[0]>adc[1])
-	{	if(adc[1]<adc[2])
-		{	if(adc[0]<adc[2])
-			{	x = adc[0];
-			}
-			else if(adc[0]>adc[2])
-			{	x = adc[2];				
-			}			
-		}			
-	}
-
-
-	if(adc[0]>adc[2])
-	{	if(adc[1]>adc[2])
-		{	if(adc[0]<adc[1])
-			{	x = adc[0];				
-			}			
-			else if(adc[0]>adc[1])
-			{	x = adc[1];			
-			}
-		}	
-		return;		
-	}
-	
-
-//### ALTE SCHLEIFE (3 ZAHLEN) ###_______________________________	
-	
-	
 // 	if(adc[0]<adc[1])
-// 	{	if(adc[1]<adc[2])
-// 		{	if(adc[0]<adc[2])
-// 			{	x = adc[1];
-// 			}			
-// 		}			
+// 	{	if(adc[0]<adc[2])
+// 		{	if(adc[1]<adc[2])
+// 			{	x = adc[1];				
+// 			}
+// 			else if(adc[1]>adc[2])
+// 			{	x = adc[2];				
+// 			}						
+// 		}				
 // 	}
-// 	
-// 	
-// 	if(adc[0]<adc[1])
-// 	{	if(adc[1]>adc[2])
-// 		{	if(adc[0]<adc[2])
-// 			{	x = adc[2];
-// 			}			
-// 		}			
-// 	}
-// 	
-// 	
-// 	
+// 
+// 
 // 	if(adc[0]>adc[1])
 // 	{	if(adc[1]<adc[2])
 // 		{	if(adc[0]<adc[2])
-// 			{	x = adc[0];		
-// 			}			
-// 		}		
-// 	}
-// 	
-// 		
-// 		if(adc[0]>adc[1])
-// 	{	if(adc[1]<adc[2])
-// 		{	if(adc[0]>adc[2])
-// 			{	x = adc[2];
-// 			}			
-// 		}			
-// 	}
-// 		
-// 		
-// 		if(adc[0]<adc[1])
-// 	{	if(adc[1]>adc[2])
-// 		{	if(adc[0]>adc[2])
 // 			{	x = adc[0];
+// 			}
+// 			else if(adc[0]>adc[2])
+// 			{	x = adc[2];				
 // 			}			
 // 		}			
 // 	}
-// 	
-// 		
-// 	if(adc[0]>adc[1])
+// 
+// 
+// 	if(adc[0]>adc[2])
 // 	{	if(adc[1]>adc[2])
-// 		{	if(adc[0]>adc[2])
-// 			{	x = adc[1];
+// 		{	if(adc[0]<adc[1])
+// 			{	x = adc[0];				
 // 			}			
-// 		}			
+// 			else if(adc[0]>adc[1])
+// 			{	x = adc[1];			
+// 			}
+// 		}	
+// 		return;		
 // 	}
+	
 
 //### ALTE SWAP-SCHLEIFE###___________________________
-/*
-if(adc[0] > adc[1])
+
+void median(void)
 {
-	swap(adc[0], adc[1]);
+	if(adc[0] > adc[1])
+{
+	swap(&adc[0], &adc[1]);
 }
 
 if(adc[2] > adc[3])
 {
-	swap(adc[2], adc[3]);
+	swap(&adc[2], &adc[3]);
 }
 
 if(adc[1] > adc[3])
 {
-	swap(adc[0], adc[2]);
-	swap(adc[1], adc[3]);
+	swap(&adc[0], &adc[2]);
+	swap(&adc[1], &adc[3]);
 }
 
 if(adc[4] < adc[1])
 {
 	if(adc[4] < adc[0])
 {
-	swap(adc[0], adc[4]);
-	swap(adc[4], adc[1]);
-	swap(adc[4], adc[3]);
+	swap(&adc[0], &adc[4]);
+	swap(&adc[4], &adc[1]);
+	swap(&adc[4], &adc[3]);
 }
-	else if(adc[4] < adc [3])
+}
+	else 
+	if(adc[4] < adc [3])
 	{
-		swap(adc[3], adc[3]);
-}	}
-
-if(adc[2] < adc[1])
-{	if(adc[2] < adc[0])
-	{
-		swap(adc[0], adc[2]);
-		swap(adc[1], adc[2]);
-	}		
-	else if(adc[2] > adc[3])
-	{
-		swap(adc[2], adc[3]);
+		swap(&adc[4], &adc[3]);
 	}
-}		
-*/
+
+if(adc[2] < adc[1]) swap(&adc[2], &adc[1]);
+if(adc[2] < adc[0])	swap(&adc[2], &adc[0]);
+// {	if(adc[2] < adc[0])
+// 	{
+// 		swap(&adc[0], &adc[2]);
+// 		swap(&adc[1], &adc[2]);  //swap(&adc[1], &adc[2]);
+// 	}	
+// 	}		
+	else 
+	if(adc[2] > adc[3])
+	{
+		swap(&adc[2], &adc[3]);
+	}
+
+/*if(adc[1] > adc[2]) swap(&adc[1], &adc[2]);*/
+return;
 		}
-
-
-
 
 
 
@@ -217,23 +143,25 @@ int main(void)
 {
 	init();
 	uint8_t i;
-/*	int t;*/
+	uint8_t t=0;
+	uint16_t k=1500;
 /*	int temp;*/
-	uint8_t timer;
+/*	uint8_t timer*/;
     while(1)
     {
-        _delay_ms(1000);
-	
-		for(i=0;i<5;i++) uartputc(adc[i]);
+		uartputc('\n');
+		uartputs("Array:");
+		for(i=0;i<5;i++) {uartput_uint16(adc[i]); uartputc(' ');}
 		 uartputc('\n');
-/*		median();*/	
-		for(timer=0;timer<5;timer++) Bubble_Sort_Long();
-		//Bubble_Sort_Long();
-		//swap(&adc[0], &adc[1]);
-		//uartputc(temp);
-		//uartputc(x);
-		uartputc(adc[2]);
+		uartputs("Median:");
+		uartput_uint16(adc[2]);
 		uartputc('\n');
+		if(adc[2]>=k) t++;
+		else t--;
+		if(t>3) uartputs("Warnung, Ueberstrom!");
+		median();	
+		//for(i=0;i<5;i++) uartput_uint16(adc[i]);	
 		uartputc('\n');
+		_delay_ms(3000);
     }
 }
