50f7849f019aba29c35dc7cdc0fd631b60c1d623
Tim Herz <t.herz@gsi.de>
2012-11-21 09:27:26 +0100

    adc.h finished adc.c finished

---
+++ adc.c
@@ -0,0 +1,26 @@
+﻿
+#include <avr/io.h>
+
+void ADC_init (void)
+{
+	uint16_t wert;
+	
+	ADMUX	= (0<<REFS1)|(1<<REFS0);
+	ADCSRA	= (1<<ADPS1)|(1<<ADPS0); 
+	ADCSRA |= (1<<ADEN);
+	
+	ADCSRA |= (1<<ADSC);
+	while (ADCSRA & (1<<ADSC)){}
+	wert	= ADCW;
+	
+	return;
+}
+
+uint16_t ADC_Read (uint8_t channel)
+{
+	ADMUX	= (ADMUX & ~(0x0F)) | (channel & 0x0F);
+	ADCSRA |= (1<<ADSC);
+	while (ADCSRA & (1<<ADSC)){}
+		
+	return ADCW;	
+}
---
+++ adc.h
@@ -0,0 +1,15 @@
+﻿/*
+ * IncFile1.h
+ *
+ * Created: 05.11.2012 10:43:17
+ *  Author: therz
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+void ADC_init (void);
+uint16_t ADC_Read (uint8_t channel);
+
+
+#endif /* ADC_H_ */
