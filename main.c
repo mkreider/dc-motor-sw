9f9afe71c727104051dfc4052e780a01da718e41
Burak karadeniz <b.karadeniz@gsi.de>
2012-12-11 17:45:44 +0100

    main edited

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 /*
- * main.c
+ * Projekt DC Motorsteuerung
  *
+ * Projektleiter: Herz Tim
+ * Nadja Fischer
+ * Burkhard Philip 
+ * Karadeniz Burak
+ *
+ * GSI Helmholtszzentrum für Schwerionenforschung GmbH
+ * 64291 Darmstadt
+ * Planckstraße 1
+ * 
+ 
+ * main.c
+  
  * Created: 13.11.2012 13:45:06
  *
  * Last edited on: 21.11.2012 P.B.
@@ -13,22 +25,49 @@
 #include "adc.h"
 #include "median.h"
 #include "uart.h"
+#include "int.h"
+#include "error.h"
 
-
-
-
-void init(void)
-{	
-	DDRA = 0b01110000;			//I/O's fuer die DC-Motorsteuerung
-	DDRB = 0b11011111;			// Pull-Ups???
+		
+		
+		
+						//* Notizen:
+						
+							//  Power-Up Timer im Init nötig?
+							//  Jedes Interrupt routinen mit Abfrage zu nFault beenden.
+							
+		
+		
+		
+		
+		
+		
+		
+void init(void)			
+{			
+										//* Einfügen der benötigten Konstanten und Variablen
+	uint8_t error = 0x00;						// Error Register zur Fehlererkennung								
+	
+	
+	DDRA = 0b01110000;					//* Inputs / Outputs		
+	DDRB = 0b11011111;							// Pull-Ups???
 	DDRC = 0b00000000;
 	DDRD = 0b11000111;		
-							
+						
 	
-// 	DDRB = 0b01111111;				
-// 	DDRC  = 0x00;
-// 	PORTC &= ~(1<<PC0);
-	init_uart();
+	init_uart();						//* Rufe UART init auf
+	ADC_init ();						//* Rufe ADC init auf
+	
+	
+	if (PINC & (1<<PC7))				//* Motorerkennung //
+	{											// Wenn Motor vorhanden dann rufe Interrupt init auf	
+	Interrupt_init ();							// Wenn nicht dann setze Pin0 des Error Registers auf 1
+	}											
+	else
+	{
+	 error |= (1<<0);							
+	 }
+	
 	
 	return;		
 }
@@ -38,33 +77,43 @@ void init(void)
 
 int main(void)
 {
-	init();
-	uartputs("Hallo Welt\n");
+	init();								//* Rufe init auf
 		
 
-		
-	
     while(1)
     {	
-		if(1)
-		{
-			test_uartgetc();
-		}
 		
-		//TODO:: Please write your application code
-		//uartputc(uart_getc());
+		
+	if (PC7 > 0)						//* nFault Prüfung						
+	{											// Wenn nFault Fehler meldet dann setze Pin1 des Error Registers auf 1 
+		error |= (1<<1)
+	}
+		
+	
+	if (error >  0)						//* Fehlererkennung
+	{											// Wenn ein Fehler vorliegt dann rufe Error init auf
+		error_init ();							// Wenn nicht dann gehe weiter
+	}
+		
+		
+	if (PA4 == 1)
+	{
+		// Remote Vorgang
+	} 
+	
+	
+		
 		
 
 	}    
         
-		
-
-
-
-		
+	
+	
 		
 }
     
+
+
 
 
 
